dbc1e0d397a54235464560d31702c0cdb1a80cff
Marcos Ibañez <mgi1982@gmail.com>
2012-12-16 01:41:21 -0300

    Testing composer on azure

--- build_azure.php
+++ build_azure.php
@@ -1,7 +1,7 @@
 <?php
-if (!file_exists("composer.phar")) {
+if (!file_exists($_SERVER['DEPLOYMENT_SOURCE'] . "/composer.phar")) {
     $url = 'https://getcomposer.org/composer.phar';
-    file_put_contents("composer.phar", file_get_contents($url));
+    file_put_contents($_SERVER['DEPLOYMENT_SOURCE'] . "/composer.phar", file_get_contents($url));
 }
 
 register_shutdown_function('copyFiles');
@@ -12,7 +12,7 @@ echo var_export($_SERVER);
 $_SERVER['argv'][1] = "update";
 $_SERVER['argv'][2] = "--prefer-dist";
 $_SERVER['argv'][3] = "-v";
-require "composer.phar";
+require $_SERVER['DEPLOYMENT_SOURCE'] . "/composer.phar";
 
 function copyFiles()
 {
