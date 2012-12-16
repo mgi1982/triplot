c3f98754083ebc6c55408cdc6d6c3a0a484c2229
casivaagustin <casivaagustin@gmail.com>
2012-12-16 04:04:41 -0300

    Dev Work Done

--- src/src/Triplot/TriplotBundle/Resources/public/js/triplot.js
+++ src/src/Triplot/TriplotBundle/Resources/public/js/triplot.js
@@ -1,6 +1,7 @@
 var map = null;
 var markers = [];
-    
+var slideshow_stop = false;
+
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(40.7, -73.8),
@@ -12,27 +13,34 @@ function initialize() {
     renderMarkers();
 }
 
-function renderMarkers() {
+function renderMarkers(date) {
     
     var infowindow = new google.maps.InfoWindow({
         content: ''
     });
+
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+
+    markers = [];
     
     for(var index in fotos) {
-        var latLng = new google.maps.LatLng(fotos[index].latitude, fotos[index].longitude);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-        });
+        if (typeof(date) === 'undefined' || date === '' || fotos[index].date == date) {
+            var latLng = new google.maps.LatLng(fotos[index].latitude, fotos[index].longitude);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map
+            });
 
-        marker.html = '<img class="info_picture" src="/bundles/triplottriplot/pictures/' + fotos[index].file + '" />';
+            marker.html = '<img class="info_picture" src="/bundles/triplottriplot/pictures/' + fotos[index].file + '" />';
     
-        
-        markers.push(marker);
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(this.html);
-            infowindow.open(map, this);
-        });
+            markers.push(marker);
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(this.html);
+                infowindow.open(map, this);
+            });
+        }
     }
 }
     
@@ -46,7 +54,45 @@ function AutoCenter() {
     //  Fit these bounds to the map
     map.fitBounds(bounds);
 }
+
+function slideshow() {
+    if (slideshow_stop) {
+        return;
+    }
+    
+    var timeout = 2000;
+    var day0 = $('#timeline').children()[0];
+    var nextDay = $('.day.selected').next();
+    
+    if (nextDay.length == 0) {
+       $('#timeline').find('li.day').removeClass('selected');
+       renderMarkers($(day0).attr('data')); 
+       $(day0).addClass('selected'); 
+    } else {
+       $('#timeline').find('li.day').removeClass('selected');
+       renderMarkers($(nextDay).attr('data')); 
+       $(nextDay).addClass('selected'); 
+    }
+    setTimeout(slideshow, timeout); 
+}
     
 $(document).ready(function(){
     initialize();
+    $('#timeline').find('.day').click(function(){
+       $('#timeline').find('li.day').removeClass('selected');
+       renderMarkers($(this).attr('data')); 
+       $(this).addClass('selected');
+    });
+    $('#timeline').find('.slideshow').click(function(){
+       if ($(this).text() == 'Stop') {
+           $(this).text('Start').removeClass('playing');
+          slideshow_stop = true; 
+          $('#timeline').find('li.day').removeClass('selected');
+          renderMarkers();
+       } else {
+          $(this).text('Stop').addClass('playing');
+          slideshow_stop = false; 
+          slideshow();
+       }
+    });
 });
